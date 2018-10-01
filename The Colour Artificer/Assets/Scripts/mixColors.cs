41eefcbabf6a8c84861ee00c1c7b2009b3a40acc
Brandon Hernandez <starkiller127.bh@gmail.com>
2018-10-01 11:35:45 -0700

    Created script for adding colors, need "formula" to mix colors

---
+++ The Colour Artificer/Assets/Scripts/mixColors.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mixColors : MonoBehaviour {
+    
+    //includes other elemnts besides colors
+    public int numOfAddedColors = 0;
+    public ArrayList addedColors = new ArrayList();
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnMouseDown() {
+        //Formula to create new colors
+        if(numOfAddedColors > 1) {
+            //ADD formula
+        }
+    }
+}
