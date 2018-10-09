2155680a2f151370e9d51deaa9e8c8c47a6098cf
Justin-A-24 <35875932+Justin-A-24@users.noreply.github.com>
2018-10-09 11:34:39 -0700

    Addition of unity fungus to unity. addTest and mixTest scripts have been made to test mechanics, but not working properly.

---
+++ The Colour Artificer/Assets/Scripts/addTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class addTest : MonoBehaviour {
+
+    public GameObject otherObject;
+    public Button colourButton;
+    public Text firstText, secondText, mixText;
+    //public bool isRed, isBlue, isGreen;
+    //public bool redOff, blueOff, greenOff;
+
+    public mixTest mix;
+
+    // Use this for initialization
+    void Start () {
+        mixText.text = "Please choose 2 colours";
+
+        //will deactivate all "addTest2" scripts from all objects that carry it
+        //mixTest[] allComponents = FindObjectsOfType<mixTest>();
+        //foreach (mixTest component in allComponents)
+        //{
+        //    component.enabled = false;
+        //}
+
+        //redOff = false;
+        //blueOff = false;
+        //greenOff = false;
+
+        mix = GetComponent<mixTest>();
+
+        colourButton = GetComponent<Button>();
+    }
+
+    // Called after pressing a button
+    public void redButton_Click()
+    {
+        //firstText.text = "You have chosen red!";
+        mix.isRed = true;
+
+        colourButton.GetComponent<Button>().interactable = false;
+
+        //will activate all "mixTest" scripts from all objects that carry it
+        mixTest[] allComponents = FindObjectsOfType<mixTest>();
+        foreach (mixTest component in allComponents)
+        {
+            component.enabled = true;
+
+        }
+    }
+
+    public void blueButton_Click()
+    {
+        //firstText.text = "You have chosen blue!";
+        mix.isBlue = true;
+        colourButton.GetComponent<Button>().interactable = false;
+
+        //will activate all "mixTest" scripts from all objects that carry it
+        mixTest[] allComponents = FindObjectsOfType<mixTest>();
+        foreach (mixTest component in allComponents)
+        {
+            component.enabled = true;
+
+        }
+    }
+
+    public void greenButton_Click()
+    {
+        //firstText.text = "You have chosen green!";
+        mix.isGreen = true;
+        colourButton.GetComponent<Button>().interactable = false;
+
+        //will activate all "mixTest" scripts from all objects that carry it
+        mixTest[] allComponents = FindObjectsOfType<mixTest>();
+        foreach (mixTest component in allComponents)
+        {
+            component.enabled = true;
+
+        }
+    }
+}
---
+++ The Colour Artificer/Assets/Scripts/mixTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mixTest : MonoBehaviour {
+
+    public GameObject buttonObject;
+    public Button buttonColour;
+
+    public bool isRed, isBlue, isGreen;
+
+    public addTest add;
+
+
+    void Awake()
+    {
+        isRed = false;
+        isBlue = false;
+        isGreen = false;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        //addTest[] allComponents = FindObjectsOfType<addTest>();
+        //foreach (addTest component in allComponents)
+        //{
+        //    component.enabled = false;
+        //}
+        isRed = false;
+        isBlue = false;
+        isGreen = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+        if (isRed == true && isBlue == true || isRed == true && isGreen == true || isBlue == true && isGreen == true)
+        {
+
+            addTest[] allComponents = FindObjectsOfType<addTest>();
+            foreach (addTest component in allComponents)
+            {
+                component.enabled = false;
+            }
+        }
+
+    }
+
+
+}
