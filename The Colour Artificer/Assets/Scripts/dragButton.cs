8bf07abf53e45d8e46973f04dac758130ef7a3b1
Brandon Hernandez <starkiller127.bh@gmail.com>
2018-10-31 20:21:05 -0700

    Added guidebook button

--- The Colour Artificer/Assets/Scripts/dragButton.cs
+++ The Colour Artificer/Assets/Scripts/dragButton.cs
@@ -14,12 +14,7 @@ public class dragButton : MonoBehaviour, IDragHandler, IEndDragHandler {
 	}
 	
 	void Update () {
-        //if (Input.GetMouseButtonUp(0)) {
-            //check if user placed object in mixing bowl (colliders intersect)
-            
-                //hide object
-                //gameObject.GetComponent<Renderer>().enabled = false;
-        //}
+        
     }
     //Object follows mouse
     public void OnDrag(PointerEventData eventData) {
@@ -30,13 +25,9 @@ public class dragButton : MonoBehaviour, IDragHandler, IEndDragHandler {
         if (gameObject.GetComponent<Collider2D>().bounds.Intersects(mixingBowl.GetComponent<Collider2D>().bounds)) {
             //Counting how many things have been added
             mix.GetComponent<mixColors>().numOfAddedColors++;
-            print(mix.GetComponent<mixColors>().numOfAddedColors);
 
             //Adding things to the "mixing bowl"
             mix.GetComponent<mixColors>().addedColors.Add(gameObject.name);
-            foreach (var i in mix.GetComponent<mixColors>().addedColors) {
-                print(i);
-            }
             gameObject.SetActive(false);
         }
         transform.position = startPotsition;
