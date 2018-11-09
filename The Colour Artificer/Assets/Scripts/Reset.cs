46e8ab31333f109e72a2ce2bef3392f242eec44e
Justin-A-24 <35875932+Justin-A-24@users.noreply.github.com>
2018-11-08 18:54:09 -0800

    getting merge commit

---
+++ The Colour Artificer/Assets/Scripts/Reset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    public GameObject otherGameObject;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnMouseDown() {
+        print("Reset");
+        //0 colors added
+        otherGameObject.GetComponent<mixColors>().numOfAddedColors = 0;
+        //remove all colors from mixing bowl
+        otherGameObject.GetComponent<mixColors>().addedColors.Clear();
+        //show all objects again
+        otherGameObject.GetComponent<mixColors>().red.GetComponent<Renderer>().enabled = true;
+        otherGameObject.GetComponent<mixColors>().green.GetComponent<Renderer>().enabled = true;
+        otherGameObject.GetComponent<mixColors>().blue.GetComponent<Renderer>().enabled = true;
+    }
+}
