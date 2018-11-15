d216080ee580d7767704658b5165e637798097fc
Brandon Hernandez <starkiller127.bh@gmail.com>
2018-11-14 21:47:09 -0800

    Fixed reset duplicate

---
+++ The Colour Artificer/Assets/Scripts/newReset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class newReset : MonoBehaviour {
+
+    public GameObject mix;
+    public Button reset;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    public void ButtonReset() {
+        //0 colors/misc added
+        mix.GetComponent<mixColors>().numOfAddedColors = 0;
+        mix.GetComponent<mixColors>().numOfAddedMisc = 0;
+        mix.GetComponent<mixColors>().addedMisc = "None";
+        //Reset all draggable objects
+        foreach (GameObject draggable in mix.GetComponent<mixColors>().draggables) {
+            draggable.SetActive(true);
+        }
+        //Reset created (mixed) color
+        mix.GetComponent<mixColors>().creation.SetActive(false);
+    }
+}
