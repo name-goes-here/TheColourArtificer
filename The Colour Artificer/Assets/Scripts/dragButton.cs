40494e1458197494b2add7faa5b79d2c94a74610
Justin-A-24 <35875932+Justin-A-24@users.noreply.github.com>
2018-11-15 13:38:03 -0800

    Fixed drag and drop script to work for the game

--- The Colour Artificer/Assets/Scripts/dragButton.cs
+++ The Colour Artificer/Assets/Scripts/dragButton.cs
@@ -6,29 +6,159 @@ using UnityEngine.EventSystems;
 public class dragButton : MonoBehaviour, IDragHandler, IEndDragHandler {
 
     Vector2 startPotsition;
-    public GameObject mix;
+    public GameObject colour;
     public GameObject mixingBowl;
 
+    public int colourValue;
+
+    public mixTest mix;
+
     void Start () {
+
         startPotsition = transform.position;
+
+        colour.GetComponent<GameObject>();
+        mixingBowl.GetComponent<GameObject>();
+
 	}
 	
 	void Update () {
         
     }
+
     //Object follows mouse
     public void OnDrag(PointerEventData eventData) {
         transform.position = Input.mousePosition;
     }
+
     //Drag ended
     public void OnEndDrag(PointerEventData eventData) {
-        if (gameObject.GetComponent<Collider2D>().bounds.Intersects(mixingBowl.GetComponent<Collider2D>().bounds)) {
-            //Counting how many things have been added
-            mix.GetComponent<mixColors>().numOfAddedColors++;
+        if (colour.GetComponent<Collider2D>().bounds.Intersects(mixingBowl.GetComponent<Collider2D>().bounds)) {
 
-            //Adding things to the "mixing bowl"
-            mix.GetComponent<mixColors>().addedColors.Add(gameObject.name);
-            gameObject.SetActive(false);
+            if(colourValue == 1)
+            {
+                mix.isRed = true;
+                mix.isMagenta = false;
+                mix.isCyan = false;
+                mix.isYellow = false;
+                mix.isPink = false;
+                mix.isPurple = false;
+                mix.isLavender = false;
+                mix.isTurquoise = false;
+                mix.isCerulean = false;
+                mix.isAquamarine = false;
+                mix.isGold = false;
+                mix.isOrange = false;
+                mix.isPeach = false;
+                mix.isElation = false;
+                mix.isEnergy = false;
+                mix.isEquilibrium = false;
+                colour.SetActive(false);
+            }
+
+            else if (colourValue == 2)
+            {
+                mix.isBlue = true;
+                mix.isMagenta = false;
+                mix.isCyan = false;
+                mix.isYellow = false;
+                mix.isPink = false;
+                mix.isPurple = false;
+                mix.isLavender = false;
+                mix.isTurquoise = false;
+                mix.isCerulean = false;
+                mix.isAquamarine = false;
+                mix.isGold = false;
+                mix.isOrange = false;
+                mix.isPeach = false;
+                mix.isElation = false;
+                mix.isEnergy = false;
+                mix.isEquilibrium = false;
+                colour.SetActive(false);
+            }
+
+            else if (colourValue == 3)
+            {
+                mix.isGreen = true;
+                mix.isMagenta = false;
+                mix.isCyan = false;
+                mix.isYellow = false;
+                mix.isPink = false;
+                mix.isPurple = false;
+                mix.isLavender = false;
+                mix.isTurquoise = false;
+                mix.isCerulean = false;
+                mix.isAquamarine = false;
+                mix.isGold = false;
+                mix.isOrange = false;
+                mix.isPeach = false;
+                mix.isElation = false;
+                mix.isEnergy = false;
+                mix.isEquilibrium = false;
+                colour.SetActive(false);
+            }
+
+            else if (colourValue == 4)
+            {
+                mix.isMagenta = false;
+                mix.isCyan = false;
+                mix.isYellow = false;
+                mix.isPink = false;
+                mix.isPurple = false;
+                mix.isLavender = false;
+                mix.isTurquoise = false;
+                mix.isCerulean = false;
+                mix.isAquamarine = false;
+                mix.isGold = false;
+                mix.isOrange = false;
+                mix.isPeach = false;
+                mix.isElation = true;
+                mix.isEnergy = false;
+                mix.isEquilibrium = false;
+                colour.SetActive(false);
+            }
+
+            else if (colourValue == 5)
+            {
+                mix.isGreen = true;
+                mix.isMagenta = false;
+                mix.isCyan = false;
+                mix.isYellow = false;
+                mix.isPink = false;
+                mix.isPurple = false;
+                mix.isLavender = false;
+                mix.isTurquoise = false;
+                mix.isCerulean = false;
+                mix.isAquamarine = false;
+                mix.isGold = false;
+                mix.isOrange = false;
+                mix.isPeach = false;
+                mix.isElation = false;
+                mix.isEnergy = true;
+                mix.isEquilibrium = false;
+                colour.SetActive(false);
+            }
+
+            else if (colourValue == 6)
+            {
+                mix.isGreen = true;
+                mix.isMagenta = false;
+                mix.isCyan = false;
+                mix.isYellow = false;
+                mix.isPink = false;
+                mix.isPurple = false;
+                mix.isLavender = false;
+                mix.isTurquoise = false;
+                mix.isCerulean = false;
+                mix.isAquamarine = false;
+                mix.isGold = false;
+                mix.isOrange = false;
+                mix.isPeach = false;
+                mix.isElation = false;
+                mix.isEnergy = false;
+                mix.isEquilibrium = true;
+                colour.SetActive(false);
+            }
         }
         transform.position = startPotsition;
     }
