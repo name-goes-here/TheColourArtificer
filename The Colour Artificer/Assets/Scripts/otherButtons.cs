51a162308377d1e7e7b28fa3476634d2aba9f545
Brandon Hernandez <starkiller127.bh@gmail.com>
2018-11-28 23:44:15 -0800

    Added page turning to guidebook

--- The Colour Artificer/Assets/Scripts/otherButtons.cs
+++ The Colour Artificer/Assets/Scripts/otherButtons.cs
@@ -8,26 +8,49 @@ public class otherButtons : MonoBehaviour {
     public bool guidebookOpen = false;
     public GameObject guidebookButton;
     public GameObject guidebookWindow;
+    public GameObject previous;
+    public GameObject next;
+    public int pageNum = 1;
 
     void Start () {
-        guidebookWindow.gameObject.SetActive(false);
+        
     }
 	
 	void Update () {
 		
 	}
 
-    public void guidebook_click() {
+    public void guidebookClick() {
         if (guidebookOpen) {
             //Gudiebook was open, so close it
             guidebookOpen = false;
             guidebookWindow.gameObject.SetActive(false);
+            previous.gameObject.SetActive(false);
+            next.gameObject.SetActive(false);
             guidebookButton.GetComponentInChildren<Text>().text = "Gudiebook";
         } else {
             //Guidebook was closed, so open it
             guidebookOpen = true;
             guidebookWindow.gameObject.SetActive(true);
+            previous.gameObject.SetActive(true);
+            next.gameObject.SetActive(true);
             guidebookButton.GetComponentInChildren<Text>().text = "Close";
+        }
+    }
+
+    public void previousPage() {
+        if(pageNum > 1) {
+            pageNum--;
+            guidebookWindow.transform.Find(pageNum.ToString()).gameObject.SetActive(true);
+            guidebookWindow.transform.Find((pageNum + 1).ToString()).gameObject.SetActive(false);
+        }
+    }
+
+    public void nextPage() {
+        if(pageNum < 3) {
+            pageNum++;
+            guidebookWindow.transform.Find(pageNum.ToString()).gameObject.SetActive(true);
+            guidebookWindow.transform.Find((pageNum - 1).ToString()).gameObject.SetActive(false);
         }
     }
 }
