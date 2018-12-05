2ab54d2f90c7dff390b4f8f51d92f46907116e52
Brandon Hernandez <starkiller127.bh@gmail.com>
2018-12-04 23:19:24 -0800

    Loading Screen testing

---
+++ The Colour Artificer/Assets/Scripts/loadingScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadingScreen : MonoBehaviour {
+
+    //AsyncOperation async;
+    //int count = -1;
+
+    public void loading() {
+        SceneManager.LoadScene("Loading Screen");
+    }
+
+    /*  TESTING FOR ACTUAL LOADING SCREEN ASYNC PROCESS
+    IEnumerator LoadingScreen() {
+        //Switch to loading screen
+        SceneManager.LoadScene("Loading Screen");
+        async = SceneManager.LoadSceneAsync(2);
+        async.allowSceneActivation = false;
+        print(3);
+        while (async.isDone == false) {
+            print(4);
+            //Is scene or level fully loaded
+            if (async.progress == 0.9f) {
+                print(5);
+                yield return new WaitForSecondsRealtime(3);
+                print("Waited");
+                async.allowSceneActivation = true;
+            }
+            print(6);
+            yield return null;
+        }
+    }*/
+}
